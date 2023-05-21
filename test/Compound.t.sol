174021982180ef094fd4535277b281138fedd33a
alan890104 <alan890104@gmail.com>
2023-05-21 12:57:43 +0800

    feat: add infrastructure

---
+++ test/Compound.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract TestCompoundUSDC is Test {
+    function setUp() public {
+        
+    }
+
+    function testAliceProvidesLiquidityAndBobWithdraws() public {
+
+    }
+}
